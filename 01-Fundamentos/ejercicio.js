204658321be36ac32f6c4c1501fc39f5cd147c54
maik mercado <maikmercadom@gmail.com>
2022-02-08 16:18:57 -0300

    sentencias 08-02-2022

---
+++ 01-Fundamentos/ejercicio.js
@@ -0,0 +1,16 @@
+//ejercicio calcular el IMC indice de masa corporal)
+
+var peso = 50;
+var altura = 1.75;
+
+
+var peso = 80;
+var altura = 1.75;
+
+var pedro = peso /(altura * altura);
+var juan = peso  /(altura * altura);
+
+
+var imc = juan < pedro;
+
+console.log("el IMC es juan es mayor a predro ? " + imc)
